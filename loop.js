0d7ea7501d55b41147ba0124c509227877538f7a
Garrett James <garrettjames55@gmail.com>
2019-10-22 11:33:39 -0400

    added max and min

---
+++ loop.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function max(numbers) {
+  let i = 0;
+  let max = numbers[0];
+  while(i < numbers.length) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+    i++;
+  }
+  return max;
+}
+
+function min(numbers) {
+  let i = 0;
+  let min = numbers[0];
+  while(i < numbers.length) {
+    if (numbers[i] < min) {
+      min = numbers[i];
+    }
+    i++;
+  }
+  return min;
+}
